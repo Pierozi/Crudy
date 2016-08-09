32b313c901dc825c6a9e0218eb2c6a2728e203d0
Pierre Tomasina <tomasina@plab.io>
2016-08-09 14:59:40 +0200

    add test for delete method

---
+++ tests/api/Resources/DeleteCept.php
@@ -0,0 +1,18 @@
+<?php
+
+$I = new ApiTester($scenario);
+$I->wantTo('Delete resource');
+$I->haveHttpHeader('ACCEPT', 'application/vnd.api+json');
+$I->haveHttpHeader('Content-Type', 'application/vnd.api+json');
+
+$data = json_encode([
+    "data" => [
+        "type"       => "articles",
+        "attributes" => [
+            "isDelete" => true,
+        ],
+    ],
+]);
+
+$I->sendDELETE('/articles/42', $data);
+$I->seeResponseCodeIs(204);
