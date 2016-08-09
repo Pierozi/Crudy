5c6d1f0d6da3517143c03634a00e9a11f0290605
Pierre Tomasina <tomasina@plab.io>
2016-08-09 14:25:01 +0200

    add test for resource read method

--- public/Resources/Articles/Read.php
+++ public/Resources/Articles/Read.php
@@ -8,9 +8,27 @@ class Read extends Resource implements ReadInterface
 {
     public function read(string $resourceId)
     {
+        if ('42' !== $resourceId) {
+            $this->notFound();
+        }
+
+        $this->createResource($resourceId, [
+            'foo' => 'bar',
+            'baz' => true,
+        ]);
     }
 
     public function readAll()
     {
+        $this->createResource(1, [
+            'foo'   => 'bar',
+            'baz'   => false,
+            'dummy' => 'lipsum'
+        ]);
+
+        $this->createResource(42, [
+            'foo' => 'bar',
+            'baz' => true
+        ]);
     }
 }
